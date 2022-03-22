8e670648162d6408c3cc03927c06c48cda9e3b61
Katriin Poolakese <katriin.poolaese@khk.ee>
2022-03-22 18:57:30 +0200

    Created connection to database

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ app.use(express.json());
 // parse request of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true}));
 
+// connect to database
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize');
+
+// testing connection
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log('Connected to the database');
+	})
+	.catch(err => {
+		console.error('Unable to connect to the database', err);
+	});
+
 // simple route
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to sequelize application."});
