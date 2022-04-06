c3f8055c8eaed7f8db6400a6103262e381dfeea9
Katriin Poolakese <katriin.poolaese@khk.ee>
2022-04-06 19:14:29 +0300

    Created API for tags information by tag name and id

--- controllers/api/tags.js
+++ controllers/api/tags.js
@@ -4,40 +4,40 @@ const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/yoga_sequeli
 // read model data for table representation
 const models = require('../../models')
 
-// show author by name or id
-const getAuthor = (req, res) => {
+// show article tags
+const getTags = (req, res) => {
 	let queryElement = Object.keys(req.query)
 	if(Object.keys(req.query).length === 0){
 		return res.status(400).json({ 'error': 'Invalid request'});
-	}else if(req.query.author_name === ''){
+	}else if(req.query.tag_name === ''){
 		return res.status(400).json({ 'error': 'Invalid request'});
 	}else if(req.query.id === ''){
 		return res.status(400).json({ 'error': 'Invalid request'});
 	} else {
-	 if(queryElement[0] === 'author_name') {
-		models.Author.findOne({
+	 if(queryElement[0] === 'tag_name') {
+		models.Tags.findOne({
 			where: {
-				name: req.query.author_name
+				name: req.query.tag_name
 			},
 			include: [{
 				model: models.Article
 			}],
 		})
-		.then(article => {
-			return res.status(200).json({ article });
+		.then(tags => {
+			return res.status(200).json({ tags });
 		})
 		.catch (error => {
 			return res.status(500).send(error.message);
 		})
 	} if(queryElement[0] === 'id') {
-		models.Author.findByPk(req.query.id, {
+		models.Tags.findByPk(req.query.id, {
 		include: [{
 			model: models.Article
 		}],
 	})
-	.then(author => {
-		console.log(author)
-		return res.status(200).json({ author });
+	.then(tags => {
+		console.log(tags)
+		return res.status(200).json({ tags });
 	})
 	.catch (error => {
 		return res.status(500).send(error.message);
@@ -46,7 +46,7 @@ const getAuthor = (req, res) => {
 	}
 };
 
-// export
+// export controller functions
 module.exports = {
-	getAuthor
+	getTags
 } 
--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const apiArticleController = require('../controllers/api/article');
 const apiAuthorController = require('../controllers/api/author');
+const apiTagController = require('../controllers/api/tags');
 
 router.get('/article', apiArticleController.getArticle);
 router.get('/author', apiAuthorController. getAuthor);
+router.get('/tag', apiTagController.getTags);
 
 // export router
 module.exports = router;
