432f381838926433919969373fff0cc645109441
Katriin Poolakese <katriin.poolaese@khk.ee>
2022-04-06 18:38:03 +0300

    Update article data

--- routes/article.js
+++ routes/article.js
@@ -7,5 +7,6 @@ router.get('/', articleController.getAllArticles);
 router.get('/article/:slug', articleController.getArticleBySlug);
 
 router.post('/admin/article/create', articleAdminController.createArticle);
+router.post('/admin/article/edit/:id', articleAdminController.updateArticle);
 
 module.exports = router;
